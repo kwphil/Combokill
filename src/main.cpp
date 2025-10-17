aab110bf695eff1d345bfa288bec477c4e624af5
kwphil <phillipsw1980@gmail.com>
2025-10-16 19:51:29 -0500

    Opened a window

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,53 @@
-#include <cstdlib>
 #include <iostream>
 
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_video.h>
+#include <SDL3/SDL_error.h>
+#include <SDL3/SDL_render.h>
+#include <SDL3/SDL_events.h>
+
 int main() {
-	std::cout << "Hello, World!";
-	return EXIT_SUCCESS;
+        SDL_Init(SDL_INIT_VIDEO);
+
+        SDL_Window* window = SDL_CreateWindow("Combokill", 800, 600, 
+                SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL
+        );
+
+        SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+
+        if(!window) {
+                std::cerr << SDL_GetError() << std::endl;
+                SDL_Quit();
+                return EXIT_FAILURE;
+        }
+
+        if(!renderer) {
+                std::cerr << SDL_GetError() << std::endl;
+                SDL_Quit();
+                return EXIT_FAILURE;
+        }
+
+        SDL_Event event;
+        bool running = true;
+
+        while(running) {
+                while(SDL_PollEvent(&event)) {
+                        if(event.type == SDL_EVENT_QUIT) {
+                                running = false;
+                        }
+                }
+
+                SDL_UpdateWindowSurface(window);
+                SDL_RenderClear(renderer);
+                SDL_RenderPresent(renderer);
+
+                SDL_Delay(16);
+        }
+
+        std::cout << "Exiting successfully";
+        SDL_DestroyWindow(window);
+        SDL_DestroyRenderer(renderer);
+        SDL_Quit();
+        return EXIT_SUCCESS;
 }
 

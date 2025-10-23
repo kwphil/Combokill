eef9a964c7ecfb55b0b4e448013af2d0ddd1d8c3
zachariahstauffer <100742502+zachariahstauffer@users.noreply.github.com>
2025-10-23 18:54:28 -0500

    Enhance comments for Camera class in camera.hpp
    Added comments to clarify the purpose of class members and methods in camera.hpp.

--- src/camera.hpp
+++ src/camera.hpp
@@ -1,18 +1,19 @@
 #pragma once
+//prevents duplication
 
-#include "object.hpp"
+#include "object.hpp" //includes contents from object.hpp
 
-class Camera : public Object {
+class Camera : public Object { //creates a class and a object that can be used anywhere
 private:
-        glm::vec3 normalize_right();
+        glm::vec3 normalize_right(); //calculates right vector and gives it a value of 1 for only this file
 
 public:
-        glm::vec3 look_at;
+        glm::vec3 look_at; //coordinants of what the camera's posision is
 
-        Camera(glm::vec3 origin, glm::vec3 look_at);
-        Camera(const Camera& other);
-        const glm::vec3 get_look_at() const { return look_at; }
-        void strafe(float speed);
+        Camera(glm::vec3 origin, glm::vec3 look_at); //uses 3 vectors to figure out what the posiston of the camera is and what it is lookig at
+        Camera(const Camera& other); //coppies an existing camera constructor
+        const glm::vec3 get_look_at() const { return look_at; } //retrieves 3d vector
+        void strafe(float speed); //camera's movement speed
 };
 
-extern void move_camera();
+extern void move_camera(); //lets the compiler know move_camera() is on a different file
